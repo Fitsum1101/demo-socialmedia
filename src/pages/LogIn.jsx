03bc08aeeb7cd59a9f3173b1402cfd0733a2fa2a
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-12 13:03:20 +0300

    sumbting data dones successfuly

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -5,3 +5,34 @@ const LogIn = () => {
 };
 
 export default LogIn;
+
+export const action = async ({ request, params }) => {
+  try {
+    const formData = await request.formData();
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const result = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    if (result.status === 422) {
+      throw new Error("invalide user input");
+    }
+    // if (result.status !== 200 || result.status !== 201) {
+    //   throw new Error("user not authticated");
+    // }
+    const data = await result.json();
+    localStorage.setItem("token", data.token);
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import Signup from "../compenents/Auth/Signup";
+import { redirect } from "react-router";
 const SignUp = () => {
   return <Signup />;
 };
 
 export default SignUp;
+
+export const action = async ({ request, params }) => {
+  const datas = await request.formData();
+  const username = datas.get("username");
+  const email = datas.get("email");
+  const password = datas.get("password");
+  const result = await fetch("http://localhost:8080/signUp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  if (!result.ok) {
+    const data = await result.json();
+    if (result.status === 422) {
+      return "Email aiready in use!!";
+    }
+    return "server is not working aproprately";
+  }
+  return redirect("/login");
+};
