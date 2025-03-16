99db6de40879cd485c6526973d52e2b669b971bd
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-16 17:35:43 +0300

    for handling onclick

--- src/compenents/button/Button.js
+++ src/compenents/button/Button.js
@@ -1,15 +1,16 @@
 import React from "react";
 import classes from "./Button.module.css";
 import { Link } from "react-router";
-const Button = ({ children, type, handleClick, ref, onClick }) => {
+const Button = ({ children, type, handleClick, ref, onClick, to }) => {
   return (
     <button
       ref={ref}
       type={type}
-      // onClick={() => handleClick()}
+      onClick={handleClick ? () => handleClick() : handleClick}
       className={classes.btn}
+      // type="submit"
     >
-      <Link>{children}</Link>
+      <Link to={to}>{children}</Link>
     </button>
   );
 };
